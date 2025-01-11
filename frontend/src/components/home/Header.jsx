ec0674b4575d41b8d5767a10ad1c7977f8c34a28
adityapwr87 <adityapwr87@gmail.com>
2025-01-11 19:16:47 +0530

    home page aditya

---
+++ frontend/src/components/home/Header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">FleetFlex</div>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.instagram.com/_aditya_pwr/">
+              <img src="./icons/instagram.svg" alt="Instagram" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="/icons/linkedin.svg" alt="LinkedIn" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="/icons/x-icon.svg" alt="X (Twitter)" />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="home-button">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="contact-button">
+              Contact us
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/components/home/Main.jsx
@@ -0,0 +1,28 @@
+// src/components/Main.js
+import React from "react";
+import "./Main.css"; // Import Main CSS
+
+function Main() {
+  return (
+    <main className="main">
+      <div className="main-content">
+        <h1>Welcome to FleetFlex</h1>
+        <p>
+          FleetFlex connects you with vehicles tailored to your needs, whether
+          for passenger transport or goods delivery. 
+          Join us for seamless transportation services!
+        </p>
+        <div className="buttons">
+          <button onClick={() => (window.location.href = "/signup")}>
+            SIGN UP
+          </button>
+          <button onClick={() => (window.location.href = "/login")}>
+            SIGN IN
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
